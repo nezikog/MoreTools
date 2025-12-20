76d1f39f15dad3eca763699643991d2e36c5138b
NagiFish <94735845+nezikog@users.noreply.github.com>
2025-12-20 12:59:19 +0300

    Update qr.vue

---
+++ vue-tools/src/pages/qr.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import ApisPage from '@/components/UI/ApisPage.vue';
+</script>
+
+<template>
+    <div class="content">
+        <ApisPage></ApisPage>
+    </div>
+</template>
+
+<style lang='scss' scoped>
+    @import '../assets/styles/mixins';
+    @import '../assets/styles/variables';
+
+    // .content{
+    //     width: 95%;
+    //     height: 24rem;
+    //     background-color: $white;
+    //     border-radius: 2em;
+    //     padding: 5em;
+    // }
+
+
+
+</style>
